d5849d407e0d27fb971a4353dfe652e6c87b3373
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-11-27 21:09:42 +0530

    first commit

--- dom.js
+++ dom.js
@@ -1,3 +1,11 @@
 
 
-console.dir(document.body.childNodes);
+let button = document.getElementById("myId")//h1 
+console.dir(button);
+
+let headings = document.getElementsByClassName("myClass")
+console.dir(headings);
+console.log(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.dir("parahs");
