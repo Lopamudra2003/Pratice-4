41e4ce6c88eae91a6d70df6f04de30df64ee00ab
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-11-29 00:38:08 +0530

    first commit

--- dom.js
+++ dom.js
@@ -1,11 +1,20 @@
 
 
-let button = document.getElementById("myId")//h1 
-console.dir(button);
+// let button = document.getElementById("myId")//h1 
+// console.dir(button);
 
-let headings = document.getElementsByClassName("myClass")
-console.dir(headings);
-console.log(headings);
+// let headings = document.getElementsByClassName("myClass")
+// console.dir(headings);
+// console.log(headings);
 
-let parahs = document.getElementsByTagName("p");
-console.dir("parahs");
+// let parahs = document.getElementsByTagName("p");
+// console.dir("parahs");
+
+// let element = document.querySelector("p");
+// console.dir(element);
+
+let firstEl = document.querySelector(".myClass");
+console.dir(firstEl);
+
+let allEl = document.querySelector(".myClass");
+console.dir(allEl);
