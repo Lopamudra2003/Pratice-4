906c6fa7d2d360827f1efb378e93e98128a14bd7
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-12-02 23:01:44 +0530

    first commit

--- dom.js
+++ dom.js
@@ -27,7 +27,7 @@
 let h2 = document.querySelector("h2");
 console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + ("welcome to apna college") ;
+h2.innerText = h2.innerText + "welcome to apna college";
 
 
 
