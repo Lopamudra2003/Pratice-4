72b0ceb1b50cc05e85ca477c91b10b713494d942
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-12-02 22:56:19 +0530

    first commit

--- dom.js
+++ dom.js
@@ -10,11 +10,25 @@
 // let parahs = document.getElementsByTagName("p");
 // console.dir("parahs");
 
-let element = document.querySelector("p");
-console.dir(element);
+//let element = document.querySelector("p");
+//console.dir(element);
 
-let firstEl = document.querySelector(".myClass");
-console.dir(firstEl);
+// let firstEl = document.querySelector(".myClass");
+// console.dir(firstEl);
 
-let allEl = document.querySelector(".myClass");
-console.dir(allEl);
+// let allEl = document.querySelector(".myClass");
+// console.dir(allEl);
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h1");
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + ("welcome to apna college") ;
+
+
+
+
