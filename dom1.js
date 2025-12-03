5c8b9599ab4fa252afe88f9849e5dcbe167e380e
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-12-03 23:53:10 +0530

    first commit

---
+++ dom1.js
@@ -0,0 +1,21 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " welcome to apna college !!";
+
+let divs = document.querySelectorAll(".box");
+
+for(div of divs){
+    // console.log(div);
+     div.innerText = 'new unique value 1 ${idx}';
+     idx++;
+
+}
+// divs [0].innerText = "New ";
+// divs [1].innerText = "First ";
+// divs [2].innerText = "Second ";
+
+
+
+
+
