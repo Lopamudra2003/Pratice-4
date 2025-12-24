ee0ef797ffa3ab62291d81686317578a3a6877ad
Lopamudra Baral <barallopamudra2003@gmail.com>
2025-12-24 18:27:08 +0530

    first commit

--- dom2.js
+++ dom2.js
@@ -1,2 +1,26 @@
-let div = document.querySelector("p");
-console.log(para.getAttribute("class"))
+//let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// div.style.backgroundcolor = "green";
+// div.style.backgroundcolor = "purple";
+// div.style.fontsize = "23px" ;
+// div.style.visibility = "hidden";
+
+// div.innerText = "HELLO !!" ;
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me!";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// div.append(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i> Hi, i am new !!</i>";
+document.querySelector("body").prepend(newHeading);
